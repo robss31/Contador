50efa99678fd7fe36f62196d2a6f7c966b2e459a
robss31 <robss31@hotmail.com>
2022-02-27 02:21:41 -0300

    atualizando o repositorio

--- asset/js/scripts.js
+++ asset/js/scripts.js
@@ -2,11 +2,21 @@ var currentNumberWrapper = document.getElementById("currentNumber")
 var currentNumber = 0;
 
 function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < 10) {
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    if (currentNumber >= 0) {
+        document.getElementById("counter").style.color = "black"
+    }
+}
+function decrement() {
+    if (currentNumber > -10) {
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    if (currentNumber < 0) {
+        document.getElementById("counter").style.color = "red"
+    }
 }
 
-function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
